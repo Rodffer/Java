2a982c6ac0de16fdc2d22fff61136ec9594f4564
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-02-27 12:50:32 -0300

    Comentarios

--- Comentarios.java
+++ Comentarios.java
@@ -4,16 +4,13 @@ public class Comentarios {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Tipos de Coment·rios."); //Coment·rio na linha do cÛdigo
+		System.out.println("Tipos de Coment√°rios."); //Coment√°rio na linha do c√≥digo
 		
-		//Coment·rio utilizado em linha unica
+		//Coment√°rio utilizado em linha unica
 		
-		/* Coment·rio utilizado 
+		/* Coment√°rio utilizado 
 		 * 	em varias Linhas;
-		 * qualquer texto aqui ser· ignorado pelo Java
-		 */
-		
-		
-	}
-	
+		 * qualquer texto aqui ser√° ignorado pelo Java
+		 */	
+	}	
 }
