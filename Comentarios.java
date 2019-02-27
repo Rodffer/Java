cf082901b726a4de8f0391c0738dd3ecd22dee76
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-02-27 12:30:43 -0300

    Comentarios em Java

---
+++ Comentarios.java
@@ -0,0 +1,19 @@
+package gitJavaRod;
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tipos de Comentários."); //Comentário na linha do código
+		
+		//Comentário utilizado em linha unica
+		
+		/* Comentário utilizado 
+		 * 	em varias Linhas;
+		 * qualquer texto aqui será ignorado pelo Java
+		 */
+		
+		
+	}
+	
+}
