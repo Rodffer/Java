ce260ad3c4018a38e01c8cb8232161a820a22949
Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-02-27 10:27:52 -0300

    Olá, Mundo! JAVA

---
+++ Ola_Mundo.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author Rodffer
+ */
+public class Ola_Mundo {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
