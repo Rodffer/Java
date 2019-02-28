9285de4868654b3ed7d661f229d50bc0d0567502
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-02-28 16:47:04 -0300

    Boolean
    Quando se tem casos que so pode receber dois valores V ou F, ON ou OFF, Sim, NÂO

---
+++ verdadeirofalso.java
@@ -0,0 +1,18 @@
+public class verdadeirofalso {
+    public static void main(String[] args) {
+
+        boolean JavaisFun = true;       //Retorna Falso
+        boolean JavaisTasty = false;    //Retorna Verdadeiro
+
+        System.out.println(JavaisFun);
+        System.out.println(JavaisTasty);
+
+        int x = 20;
+        int y = 10;
+
+        System.out.println(x > y); // se for maior retorna true se for menor false
+        System.out.println(x == 20);// se for igual retorna true se nao false
+        System.out.println(x == y); // se for igual retorna true se nao false
+
+    }
+}
