541276d3fee08b5bfcb68f8f67673ac4dbffc05d
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-02-28 12:13:28 -0300

    Math
    A classe Math permite fazer algumastarefas matemáticas

---
+++ metodoMath.java
@@ -0,0 +1,17 @@
+public class metodoMath {
+
+    public static void main(String[] args) {
+
+        int y = 5;
+        int x = 10;
+
+        //ALGUNS EXEMPLOS DO METODO MATH
+        System.out.println(Math.min(x, y)); //Encontrar o menor valor entre x e y
+        System.out.println(Math.max(x, y)); //Encontrar o maior valor entre x e y
+        System.out.println(Math.sqrt(y));   //Encontrar a raiz quadrada de x
+        System.out.println(Math.random()); //Retorna um numero aleatorio entre 0 e 1
+        System.out.println(Math.decrementExact(y)); //Decrementa, ou seja y - 1
+        System.out.println(Math.incrementExact(y)); //Incrementa, ou seja y + 1
+        
+    }
+}
