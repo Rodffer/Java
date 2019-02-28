b053c8b8a8f456b8241032916cb9c5b490d2f481
Rayron Rodffer <44624642+Rodffer@users.noreply.github.com>
2019-02-28 10:55:11 -0300

    Operadores Matemáticos

---
+++ Operator.java
@@ -0,0 +1,30 @@
+public class Operator {
+
+    public static void main (String[] args){
+
+        //Operadores Matemáticos
+
+     // + Adição
+     // - Subtração
+     // * Multiplicação
+     // % Retornar o restante da divisão
+     // ++ Incremento
+     // -- Decremento
+
+        // Operadores Comparação
+
+     // == Igual a
+     // != Diferente
+     // > Maior que
+     // < Menor que
+     // >= Maior ou igual a
+     // <= Menor ou igual a
+
+        // Operadores Lógicos
+
+     // && Se as duas forem verdadeiras
+     // || Retorna True se uma das afirmações forem verdadeiras
+     // ! Retorna False se o resultado for verdadeiro
+
+    }
+}
